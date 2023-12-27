61e3c0a8db1596a2b57a8aa2d970fc33243d386a
rgauna79 <rgauna@gmail.com>
2023-12-26 18:59:41 -0700

    fix baseURL ito use API

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -5,7 +5,7 @@ const instance = axios.create({
   // baseURL: "http://localhost:3000/api",
 
   //connection to backend in web service
-  baseURL: "https://task-manager-p62m.onrender.com",
+  baseURL: "https://task-manager-p62m.onrender.com/api",
   withCredentials: true,
 });
 
