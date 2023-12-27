b8727cea97227bced8eb5ff02585e97cdd825d19
rgauna79 <rgauna@gmail.com>
2023-12-26 18:10:29 -0700

    added mongodb atlas connection

--- src/db.js
+++ src/db.js
@@ -1,8 +1,19 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const URI = process.env.MONGO_URI;
+
+
+console.log(URI);
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost/tododb");
+    //Connect to a local mongodb database
+    //await mongoose.connect("mongodb://localhost/tododb");
+
+    //Connect to a mongodb Atlas
+    await mongoose.connect(URI);
     console.log(">>> DB is connected");
   } catch (error) {
     console.log(error);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import app from "./app.js";
 import { connectDB } from "./db.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 connectDB();
-app.listen(3000);
-console.log("Server on port", 3000);
+app.listen(PORT);
+console.log("Server on port", PORT);
