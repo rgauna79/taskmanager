3de1838b0e4bc5323d965b1d04b998fb7e095e2b
rgauna79 <rgauna@gmail.com>
2024-01-11 11:50:13 -0700

    changing route in app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import { FRONTEND_URL } from "./config.js";
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
+import path from "path";
 
 const app = express();
 
@@ -18,12 +19,13 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.use("/api/auth", authRoutes);
 app.use("/api", taskRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  const path = await import("path");
-  app.use(express.static("client/dist"));
+  
+  app.use(express.static(path.resolve("client", "dist")));
 
   app.get("*", (req, res) => {
     console.log(path.resolve("client", "dist", "index.html"));
