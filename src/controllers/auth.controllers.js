c6619c885834934d4c3c376670be341030671e7c
RobertG <49545278+rgauna79@users.noreply.github.com>
2024-01-12 05:15:23 +0000

    Update 3 files

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -4,17 +4,19 @@ import { createAccessToken } from "../libs/jwt.js";
 import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config.js";
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
-const baseDomain = isProduction ? ".task-manager-p62m.onrender.com" : "localhost";
+const baseDomain = isProduction
+  ? ".task-manager-p62m.onrender.com"
+  : "localhost";
 
 export const register = async (req, res) => {
   try {
     const { email, password, username } = req.body;
 
     const userFound = await User.findOne({ email });
-    
-    if (userFound) 
+
+    if (userFound)
       return res.status(400).json({
         message: ["The email is already in use"],
       });
@@ -54,12 +56,11 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  
   try {
     const { email, password } = req.body;
     const userFound = await User.findOne({ email });
 
-    if (!userFound) 
+    if (!userFound)
       return res.status(400).json({
         message: ["User not found"],
       });
@@ -67,35 +68,35 @@ export const login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, userFound.password);
 
     if (!isMatch)
-      return res.status(400).json({ 
-        message: ["Incorrect password"], 
+      return res.status(400).json({
+        message: ["Incorrect password"],
       });
 
-      const token = await createAccessToken({ id: userFound._id });
-      
-      res.cookie("token", token, {
-         httpOnly: isProduction,
-         secure: isProduction, // Set to true in production with HTTPS
-         sameSite: isProduction ? "None" : "Lax", // Set SameSite attribute to None
-      });
+    const token = await createAccessToken({ id: userFound._id });
+    console.log("Generated Token:", token);
+    res.cookie("token", token, {
+      httpOnly: isProduction,
+      secure: isProduction, // Set to true in production with HTTPS
+      sameSite: isProduction ? "None" : "Lax", // Set SameSite attribute to None
+    });
 
-      res.json({
-        id: userFound._id,
-        username: userFound.username,
-        email: userFound.email,
-        createdAt: userFound.createdAt,
-        updatedAt: userFound.updatedAt,
-      });
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
 export const logout = (req, res) => {
-  res.cookie("token", "", { 
+  res.cookie("token", "", {
     httpOnly: true,
     secure: true,
-    expires: new Date(0)
+    expires: new Date(0),
   });
   return res.sendStatus(200);
 };
@@ -116,17 +117,20 @@ export const logout = (req, res) => {
 
 export const verifyToken = async (req, res) => {
   const { token } = req.cookies;
-  console.log('token: ', token);
+  console.log("token: ", token);
   try {
     if (!token) return res.send(false);
 
     jwt.verify(token, TOKEN_SECRET, async (error, user) => {
-      if (error) return res.status(401).json({ message: "Unauthorized" });
-  
+      if (error) {
+        console.error("JWT Verification Error:", error);
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+
       const userFound = await User.findById(user.id);
-  
+
       if (!userFound) return res.status(401).json({ message: "Unauthorized" });
-  
+
       return res.json({
         id: userFound._id,
         username: userFound.username,
@@ -134,7 +138,6 @@ export const verifyToken = async (req, res) => {
       });
     });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-
 };
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,15 +6,19 @@ export const auth = (req, res, next) => {
     const { token } = req.cookies;
 
     if (!token)
-      return res.status(401).json({ message: "No token, authorization denied" });
-  
+      return res
+        .status(401)
+        .json({ message: "No token, authorization denied" });
+
     jwt.verify(token, TOKEN_SECRET, (err, user) => {
-      if (err) return res.status(401).json({ message: "Invalid token" });
+      if (err) {
+        console.error("JWT Verification Error:", err);
+        return res.status(401).json({ message: "Invalid token" });
+      }
       req.user = user;
       next();
     });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
-  
 };
