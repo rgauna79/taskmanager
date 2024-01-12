0fa3a153d009785b74c37446a89ea0b22cfafec5
RobertG <49545278+rgauna79@users.noreply.github.com>
2024-01-12 03:17:57 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,12 @@ import path from "path";
 
 const app = express();
 console.log(FRONTEND_URL);
-app.use(cors());
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+  }),
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
