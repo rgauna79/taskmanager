afd1d8cc44c54761927656fe58da95ebf4315815
RobertG <49545278+rgauna79@users.noreply.github.com>
2024-01-26 05:22:45 +0000

    token again

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -87,6 +87,7 @@ export const login = async (req, res) => {
       email: userFound.email,
       createdAt: userFound.createdAt,
       updatedAt: userFound.updatedAt,
+      token: token,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
