c13a78d784ef4eabd240a428363c0f4d4d24fd2b
Roberto Gauna <49545278+rgauna79@users.noreply.github.com>
2024-09-24 15:32:29 -0600

    testin cookies in production IOS Safari
    onrender deploy

--- src/app.js
+++ src/app.js
@@ -23,16 +23,20 @@ app.use("/api/auth", authRoutes);
 app.use("/api", taskRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve("client", "dist")));
+  app.use(express.static(path.resolve("src", "client", "dist")));
 
   app.get("*", (req, res) => {
-    console.log(path.resolve("client", "dist", "index.html"));
-    res.sendFile(path.resolve("client", "dist", "index.html"));
+    console.log(path.resolve("src", "client", "dist", "index.html"));
+    res.sendFile(path.resolve("src", "client", "dist", "index.html"));
   });
 }
 
 app.use("/", (req, res, next) => {
   res.json({ message: "Api is running" });
+    console.log(`Server listening on port ${PORT}`);
+  console.log(`Current directory: ${process.cwd()}`); // Muestra la ruta actual
+  console.log(`Serving files from: ${path.resolve('src', 'client', 'dist')}`); // Muestra la ruta de los archivos estáticos
+
 });
 
 export default app;
