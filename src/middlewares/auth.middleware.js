6b24b62971c3ee6f13586ccd7fb3e7ac727468a8
RobertG <49545278+rgauna79@users.noreply.github.com>
2024-10-09 20:51:38 +0000

    checking token

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,6 +9,7 @@ export const auth = async (req, res, next) => {
     let { token } = req.cookies;
 
     if (publicRoutes.includes(req.path)) {
+      console.log("checking token in task route");
       if (!token) {
         const authHeader = req.headers.authorization;
 
@@ -18,6 +19,7 @@ export const auth = async (req, res, next) => {
             .json({ message: "No token, authorization denied" });
         }
         token = authHeader.split(" ")[1];
+        console.log("token: ", token);
       } else {
         return next();
       }
@@ -29,17 +31,17 @@ export const auth = async (req, res, next) => {
         return res.status(401).json({ message: "Invalid token" });
       }
 
-      if (req.method === "GET" && req.path === "/auth/verify") {
-        const userFound = await User.findById(user.id);
-        if (!userFound) {
-          return res.status(401).json({ message: "User not found" });
-        }
-        return res.json({
-          id: userFound._id,
-          username: userFound.username,
-          email: userFound.email,
-        });
-      }
+      // if (req.method === "GET" && req.path === "/auth/verify") {
+      //   const userFound = await User.findById(user.id);
+      //   if (!userFound) {
+      //     return res.status(401).json({ message: "User not found" });
+      //   }
+      //   return res.json({
+      //     id: userFound._id,
+      //     username: userFound.username,
+      //     email: userFound.email,
+      //   });
+      // }
 
       req.user = user;
       next();
